5baf2b87c5211e228fc2ac31c2fd451aca984cdb
Ishmael Kay <ishmael@coretech.net.au>
2013-08-18 16:21:13 +1000

    fix for assets compilation for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,10 @@ Wandr::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.initialize_on_precompile = false
+
+  config.assets.compress = true
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
